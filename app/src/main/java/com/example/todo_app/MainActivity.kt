860457588e7d4dd9ced56cc428b6226d7cb155b8
dinhm <minhndph26733@gmail.com>
2023-09-18 23:04:52 +0700

    commit

---
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.todo_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
